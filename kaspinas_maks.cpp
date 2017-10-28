bdc217e79df1582d9e61a37aa1569bd5ef1136f8
Maksim Nikanorych <zerosetup@gmail.com>
2017-10-28 14:38:32 +0300

    fix

--- kaspinas_maks.cpp
+++ kaspinas_maks.cpp
@@ -21,15 +21,17 @@ int* sort(int abc[]){
 int main() {
 	int n, count, result=0;
 	int *abc = new int[3];
-	n=930000; abc[0] = 94; abc[1] = 23; abc[2] = 44; //40
+	//n=930000; abc[0] = 94; abc[1] = 23; abc[2] = 44; //40
 	//n=121; abc[0] = 94; abc[1] = 3; abc[2] = 4; //40
 	//n=121; abc[0] = 5; abc[1] = 3; abc[2] = 7; // 39
 	//n=121; abc[0] = 3; abc[1] = 5; abc[2] = 5; // 37*3 + 5*2 // 39 
 	//n=121; abc[0] = 3; abc[1] = 7; abc[2] = 5; // 38*3 + 7*1 // 39
-	//n=7; abc[0] = 4; abc[1] = 3; abc[2] = 2;
+	//n=7; abc[0] = 4; abc[1] = 3; abc[2] = 2;  // 3
+	//n=5; abc[0] = 2; abc[1] = 4; abc[2] = 3; //2
+	
 	//n=7; abc[0] = 5; abc[1] = 5; abc[2] = 2;
 	//n=5; abc[0] = 5; abc[1] = 3; abc[2] = 2;
-	//n=5; abc[0] = 2; abc[1] = 4; abc[2] = 3;
+	
 	/**/
 	// Ввод значений
 	cout << "Enter n:";
@@ -48,13 +50,15 @@ int main() {
 	
 	abc = sort(abc);
 	count =  n / abc[0]; 
-	int k = abc[1];
+	int k = abc[0] * abc[1];
 	while(k > 0){
-		for(int i = 0; i < abc[0]; i++) {
-			for(int j=0; j <  abc[1]; j++) {
-				if( n == count * abc[0] +  abc[0] * i + abc[1] * j) {
-					if(result == 0)
-						result = count + j; 
+		for(int i = 0; i < abc[1]; i++) {
+			for(int j = 0; j <  abc[2]; j++) {
+				if( n == count * abc[0] +  abc[1] * i + abc[2] * j) {
+					//cout << "count:" << count << " i:" << i << " j:" << j << " Result" << (count + j) << "\n";
+					if(result == 0 ) {
+						result = count + j + i; 
+					}
 					break;
 				}
 			}
