405a48b013e51ee255672a29cef2775d459d34d7
Maksim Nikanorych <zerosetup@gmail.com>
2017-10-28 12:44:20 +0300

    new

--- kaspinas_maks.cpp
+++ kaspinas_maks.cpp
@@ -9,80 +9,48 @@ int findMax(int n, int a, int b, int c) {
 	int third = (n - first*a - second*b) / c;
 	return first + second + third;
 }
-
-int findEqal(int n, int a, int b) {
-	//cout  << "n=" << n << " " << a << " " << b << "\n";
+int* sort(int abc[]){
 	int tmp;
-	int first = n / a;
-	
-	if(first * a == n)
-		return first;
-	
-	tmp = a * first;
-	while(tmp > 0) {	 
-		if(tmp + b == n) {
-	//		cout << "BREAK. first: " << first <<  " tmp:" << tmp <<"\n";
-			return first + 1;
-			break;
-		}
-		tmp = tmp - a;
-		first--;
-	}
-	return 0;
-}
-/*
- * Три цикла находят 6 возможных комбинаций перестановок a,b,c в массиве: 
- * 123, 132, 213, 231, 312, 321
- * По  каждой комбинации вызвываются функции нахождения максимально количества
- * отрезков(findMax) и количества отрезков при сложении которых получается ровно
- * исходный отрезок(findEqual) 
- */
-int combinate(int n, int abc[]) {
-	int result, max=0, equalMax=0;
-	for(int i=1; i<=3; i++) {
-		for(int j=1; j<=3; j++){
-			for(int k=1; k<=3; k++) {
-				if( (i + j == k) && (i != j) && (k !=j ) ) {
-					result = findMax(n, abc[i-1],  abc[j-1], abc[k-1]);
-					if(result >  max)
-						max = result;
-					result = findEqal(n, abc[i-1],  abc[j-1]);
-					if(result >  equalMax)
-						equalMax = result;
-				}
-				if( (i + k == j) && (i != k) && (i !=j ) ) {
-					result = findMax(n, abc[i-1],  abc[j-1], abc[k-1]);
-					if(result >  max)
-						max = result;
-					result = findEqal(n, abc[i-1],  abc[j-1]);
-					if(result >  equalMax)
-						equalMax = result;
-				}
-				if( (j + k == i) && (j != k) && (j != i ) ) {
-					result = findMax(n, abc[i-1],  abc[j-1], abc[k-1]);
-					if(result >  max)
-						max = result;
-					result = findEqal(n, abc[i-1],  abc[j-1]);
-					if(result >  equalMax)
-						equalMax = result;
-				}
+	// Сортировка abc по возрастанию 
+	for(int j = 0; j <= 2; j++) {
+		for(int i = 0; i <= 1; i++) {
+			if(abc[i + 1] < abc[i]) {
+				tmp = abc[i + 1];
+				abc[i + 1] = abc[i];
+				abc[i] = tmp;
 			}
-		}			
+		}
 	}
-	//cout << "max: " << max << " maxEqual: " << equalMax << "\n";
-	if(equalMax > 0)
-		return equalMax;
-	else
-		return max;
+	cout << "Sorted:" << abc[0] << " " << abc[1] << " " << abc[2] << "\n";
+	return abc;
 }
+
+int findMultiplicator(int x, int y) {
+	int i=1, j=1, tmp;
+	
+	for(i=1; i<x*y; i++){
+		for(j=1; j<x*y; j++){
+			if(x*i == y*j){
+				return j;
+				
+			}
+		}
+	}
+}
+
 int main() {
-	int n, abc[4];
-	//n=121; abc[0] = 94; abc[1] = 3; abc[2] = 4; //40
+	int n, result;
+	int *abc = new int[3];
+
+	n=121; abc[0] = 94; abc[1] = 3; abc[2] = 4; //40
 	//n=121; abc[0] = 5; abc[1] = 3; abc[2] = 7; // 39
+	//n=121; abc[0] = 3; abc[1] = 5; abc[2] = 5; // 37*3 + 5*2 // 39 
+
 	//n=7; abc[0] = 4; abc[1] = 3; abc[2] = 2;
 	//n=7; abc[0] = 5; abc[1] = 5; abc[2] = 2;
 	//n=5; abc[0] = 5; abc[1] = 3; abc[2] = 2;
-	/**/
+	//n=5; abc[0] = 2; abc[1] = 4; abc[2] = 3;
+	/** /
 	// Ввод значений
 	cout << "Enter n:";
 	cin >> n;
@@ -97,10 +65,24 @@ int main() {
 	cin >> abc[2];
 	/**/
 	cout << "Входные данные. n:" <<  n << " a:" << abc[0] << " b:" << abc[1] << " c:" << abc[2] <<"\n";
-	cout << "Result: " << combinate(n, abc) << "\n";
+	abc = sort(abc);
+	result =  n / abc[0]; 
+	int rem = n - n / abc[0] * abc[0] + abc[0] * abc[1];
+	//result -= rem % abc[1];
+	cout <<  "result: " << result << " rem:" << rem << "\n";
+	for(int i=0; i<abc[0] * abc[1]; i++) {
+		for(int j=0; j<abc[0] * abc[1]; j++) {
+			//for(int k=0; k<abc[0]*abc[2]; k++) {
+				if(rem == abc[0] * i + abc[1] * j) {
+					cout << "Found:" << i << " " << j << " "  <<  " sum:"  << i+j <<"\n"; 
+					result += j;
+					break;
+				}
+			//}	
+		}
+	}
+	
+	//findMultiplicator(abc[0], abc[1]);
+	cout << "Result: " << result << "\n";
 }
-
-// 121 3,4,10 120-3=117  
-//3*39 + 4=121
-
 
