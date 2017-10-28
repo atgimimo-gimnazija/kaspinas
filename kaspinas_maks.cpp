f020ada0fe7ee71e5b3b29f5a57aed44c95d4491
Maksim Nikanorych <zerosetup@gmail.com>
2017-10-28 13:52:04 +0300

    working

--- kaspinas_maks.cpp
+++ kaspinas_maks.cpp
@@ -2,13 +2,6 @@
 
 using namespace std;
 
-int findMax(int n, int a, int b, int c) {
-	//cout  << "n=" << n << " " << a << " " << b << " " << c << "\n";
-	int first = n / a;
-	int second = (n - first*a) / b;
-	int third = (n - first*a - second*b) / c;
-	return first + second + third;
-}
 int* sort(int abc[]){
 	int tmp;
 	// Сортировка abc по возрастанию 
@@ -25,32 +18,19 @@ int* sort(int abc[]){
 	return abc;
 }
 
-int findMultiplicator(int x, int y) {
-	int i=1, j=1, tmp;
-	
-	for(i=1; i<x*y; i++){
-		for(j=1; j<x*y; j++){
-			if(x*i == y*j){
-				return j;
-				
-			}
-		}
-	}
-}
-
 int main() {
-	int n, result;
+	int n, count, result=0;
 	int *abc = new int[3];
 
-	n=121; abc[0] = 94; abc[1] = 3; abc[2] = 4; //40
+	//n=121; abc[0] = 94; abc[1] = 3; abc[2] = 4; //40
 	//n=121; abc[0] = 5; abc[1] = 3; abc[2] = 7; // 39
 	//n=121; abc[0] = 3; abc[1] = 5; abc[2] = 5; // 37*3 + 5*2 // 39 
-
+	//n=121; abc[0] = 3; abc[1] = 7; abc[2] = 5; // 38*3 + 7*1 // 39
 	//n=7; abc[0] = 4; abc[1] = 3; abc[2] = 2;
 	//n=7; abc[0] = 5; abc[1] = 5; abc[2] = 2;
 	//n=5; abc[0] = 5; abc[1] = 3; abc[2] = 2;
 	//n=5; abc[0] = 2; abc[1] = 4; abc[2] = 3;
-	/** /
+	/**/
 	// Ввод значений
 	cout << "Enter n:";
 	cin >> n;
@@ -65,24 +45,26 @@ int main() {
 	cin >> abc[2];
 	/**/
 	cout << "Входные данные. n:" <<  n << " a:" << abc[0] << " b:" << abc[1] << " c:" << abc[2] <<"\n";
+	
 	abc = sort(abc);
-	result =  n / abc[0]; 
-	int rem = n - n / abc[0] * abc[0] + abc[0] * abc[1];
-	//result -= rem % abc[1];
-	cout <<  "result: " << result << " rem:" << rem << "\n";
-	for(int i=0; i<abc[0] * abc[1]; i++) {
-		for(int j=0; j<abc[0] * abc[1]; j++) {
-			//for(int k=0; k<abc[0]*abc[2]; k++) {
-				if(rem == abc[0] * i + abc[1] * j) {
-					cout << "Found:" << i << " " << j << " "  <<  " sum:"  << i+j <<"\n"; 
-					result += j;
+	count =  n / abc[0]; 
+		
+	while(count > 0){
+		for(int i = 0; i < abc[0] * abc[1]; i++) {
+			for(int j=0; j < abc[0] * abc[1]; j++) {
+				if( n == count * abc[0] +  abc[0] * i + abc[1] * j) {
+					if(result == 0)
+						result = count + j; 
 					break;
 				}
-			//}	
+			}
 		}
+		count--;
 	}
 	
-	//findMultiplicator(abc[0], abc[1]);
-	cout << "Result: " << result << "\n";
+	if(result == 0)
+		result = n / abc[0];
+	
+	cout << "Ответ: " << result << "\n";
 }
 
